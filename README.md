cabbd0b4bef7727623e2fbcb46f70854a9d5c211
SenseLogic <dev.senselogic@gmail.com>
2026-08-03 21:10:04 +0200

    % options

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ crisp_uv <input video file path> <output video file path> [<options>]
 --max-width <maximum_width=0>
 --max-height <maximum_height=0>
 --tile-size <tile_size=0>
---compression <compression=22>
+--compression <compression=20>
 --skip
 --cpu
 --cuda
@@ -66,15 +66,15 @@ crisp input_video.mp4 output_video.mp4 --model ultramix
 ```
 
 ```
-crisp input_video.mp4 output_video.mp4 --compression 22
+crisp input_video.mp4 output_video.mp4 --compression 20
 ```
 
 ```
-crisp input_video.mp4 output_video.mp4 --compression 22 --max-height 1080
+crisp input_video.mp4 output_video.mp4 --compression 20 --max-height 1080
 ```
 
 ```
-crisp input_video.mp4 output_video.mp4 --compression 22 --tile-size 400
+crisp input_video.mp4 output_video.mp4 --compression 20 --tile-size 400
 ```
 
 ## Install
--- TEST/run_uv.bat
+++ TEST/run_uv.bat
@@ -1,45 +1,45 @@
-call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_bsrgan.mp4 --model bsrgan --crop 0.2 0.2 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_bsrnet.mp4 --model bsrnet --crop 0.2 0.2 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_highfidelity.mp4 --model highfidelity --crop 0.2 0.2 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_anime.mp4 --model realanime --crop 0.2 0.2 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_digital.mp4 --model realdigital --crop 0.2 0.2 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_realesrgan.mp4 --model realesrgan --crop 0.2 0.2 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_realesrnet.mp4 --model realesrnet --crop 0.2 0.2 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_remacri.mp4 --model remacri --crop 0.2 0.2 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_ultrasharp.mp4 --model ultrasharp --crop 0.2 0.2 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_ultramix.mp4 --model ultramix --crop 0.2 0.2 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_bsrgan.mp4 --model bsrgan --crop 0.3 0.3 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_bsrnet.mp4 --model bsrnet --crop 0.3 0.3 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_highfidelity.mp4 --model highfidelity --crop 0.3 0.3 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_realesrgan.mp4 --model realesrgan --crop 0.3 0.3 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_realesrnet.mp4 --model realesrnet --crop 0.3 0.3 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_remacri.mp4 --model remacri --crop 0.3 0.3 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_ultrasharp.mp4 --model ultrasharp --crop 0.3 0.3 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_ultramix.mp4 --model ultramix --crop 0.3 0.3 0 0 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\falls_480p.mp4 OUT\falls_1152p.mp4 --crop 0.2 0.2 0.1 0.1 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\falls_480p.mp4 OUT\falls_1344p.mp4 --crop 32 32 16 16 --max-ratio 3 --skip
-call "..\crisp_uv.bat" IN\falls_480p.mp4 OUT\falls_1440p.mp4 --max-height 1440 --skip
-call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_bsrgan.mp4 --model bsrgan --skip
-call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_bsrnet.mp4 --model bsrnet --skip
-call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_highfidelity.mp4 --model highfidelity --skip
-call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_realesrgan.mp4 --model realesrgan --skip
-call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_realesrnet.mp4 --model realesrnet --skip
-call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_remacri.mp4 --model remacri --skip
-call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_ultrasharp.mp4 --model ultrasharp --skip
-call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_ultramix.mp4 --model ultramix --skip
-call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_bsrgan.mp4 --model bsrgan --skip
-call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_bsrnet.mp4 --model bsrnet --skip
-call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_highfidelity.mp4 --model highfidelity --skip
-call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_realesrgan.mp4 --model realesrgan --skip
-call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_realesrnet.mp4 --model realesrnet --skip
-call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_remacri.mp4 --model remacri --skip
-call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_ultrasharp.mp4 --model ultrasharp --skip
-call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_ultramix.mp4 --model ultramix --skip
-call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_bsrgan.mp4 --model bsrgan --skip
-call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_bsrnet.mp4 --model bsrnet --skip
-call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_highfidelity.mp4 --model highfidelity --skip
-call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_realesrgan.mp4 --model realesrgan --skip
-call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_realesrnet.mp4 --model realesrnet --skip
-call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_remacri.mp4 --model remacri --skip
-call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_ultrasharp.mp4 --model ultrasharp --skip
-call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_ultramix.mp4 --model ultramix --skip
+call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_bsrgan.mp4 --model bsrgan --crop 0.2 0.2 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_bsrnet.mp4 --model bsrnet --crop 0.2 0.2 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_highfidelity.mp4 --model highfidelity --crop 0.2 0.2 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_anime.mp4 --model realanime --crop 0.2 0.2 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_digital.mp4 --model realdigital --crop 0.2 0.2 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_realesrgan.mp4 --model realesrgan --crop 0.2 0.2 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_realesrnet.mp4 --model realesrnet --crop 0.2 0.2 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_remacri.mp4 --model remacri --crop 0.2 0.2 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_ultrasharp.mp4 --model ultrasharp --crop 0.2 0.2 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\anime_480p.mp4 OUT\anime_1440p_ultramix.mp4 --model ultramix --crop 0.2 0.2 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_bsrgan.mp4 --model bsrgan --crop 0.3 0.3 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_bsrnet.mp4 --model bsrnet --crop 0.3 0.3 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_highfidelity.mp4 --model highfidelity --crop 0.3 0.3 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_realesrgan.mp4 --model realesrgan --crop 0.3 0.3 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_realesrnet.mp4 --model realesrnet --crop 0.3 0.3 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_remacri.mp4 --model remacri --crop 0.3 0.3 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_ultrasharp.mp4 --model ultrasharp --crop 0.3 0.3 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\beach_480p.mp4 OUT\beach_1440p_ultramix.mp4 --model ultramix --crop 0.3 0.3 0 0 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\falls_480p.mp4 OUT\falls_1152p.mp4 --crop 0.2 0.2 0.1 0.1 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\falls_480p.mp4 OUT\falls_1344p.mp4 --crop 32 32 16 16 --max-ratio 3 --compression 22 --skip
+call "..\crisp_uv.bat" IN\falls_480p.mp4 OUT\falls_1440p.mp4 --max-height 1440 --compression 22 --skip
+call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_bsrgan.mp4 --model bsrgan --compression 22 --skip
+call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_bsrnet.mp4 --model bsrnet --compression 22 --skip
+call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_highfidelity.mp4 --model highfidelity --compression 22 --skip
+call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_realesrgan.mp4 --model realesrgan --compression 22 --skip
+call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_realesrnet.mp4 --model realesrnet --compression 22 --skip
+call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_remacri.mp4 --model remacri --compression 22 --skip
+call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_ultrasharp.mp4 --model ultrasharp --compression 22 --skip
+call "..\crisp_uv.bat" IN\falls_360p.mp4 OUT\falls_1440p_ultramix.mp4 --model ultramix --compression 22 --skip
+call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_bsrgan.mp4 --model bsrgan --compression 22 --skip
+call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_bsrnet.mp4 --model bsrnet --compression 22 --skip
+call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_highfidelity.mp4 --model highfidelity --compression 22 --skip
+call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_realesrgan.mp4 --model realesrgan --compression 22 --skip
+call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_realesrnet.mp4 --model realesrnet --compression 22 --skip
+call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_remacri.mp4 --model remacri --compression 22 --skip
+call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_ultrasharp.mp4 --model ultrasharp --compression 22 --skip
+call "..\crisp_uv.bat" IN\park_360p.mp4 OUT\park_1440p_ultramix.mp4 --model ultramix --compression 22 --skip
+call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_bsrgan.mp4 --model bsrgan --compression 22 --skip
+call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_bsrnet.mp4 --model bsrnet --compression 22 --skip
+call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_highfidelity.mp4 --model highfidelity --compression 22 --skip
+call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_realesrgan.mp4 --model realesrgan --compression 22 --skip
+call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_realesrnet.mp4 --model realesrnet --compression 22 --skip
+call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_remacri.mp4 --model remacri --compression 22 --skip
+call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_ultrasharp.mp4 --model ultrasharp --compression 22 --skip
+call "..\crisp_uv.bat" IN\street_360p.mp4 OUT\street_1440p_ultramix.mp4 --model ultramix --compression 22 --skip
